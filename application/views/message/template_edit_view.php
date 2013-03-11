49d27303dcae88e22efcf4bf6d0ab051d19b7c6c
tiennt <tiennt@appota.com>
2013-03-11 08:47:27 +0700

    commit all

--- application/views/message/template_edit_view.php
+++ application/views/message/template_edit_view.php
@@ -25,32 +25,34 @@
     });
 </script>
 <div id="container">
-    <h3 align="center">Cập nhật mẫu tin nhắn</h3>
-    <form action="<?=base_url();?>message/template_update" method="post">
-    
-    <table width="60%" align="center" style="margin-left: 350px; padding-top: 30px;">   
-    <?php foreach($data as $rs):?>
-        <input type="hidden" name="template_id_txt" value="<?=$rs['template_id'];?>">        
-        <tr>
-            <th>Tiêu đề</th>
-            <td><input type="text" name="title" value="<?=$rs['title'];?>" style="width: 275px;"></td>
-        </tr>
-        <tr>
-            <th>Nội dung</th>
-            <td><textarea cols="50" rows="4" class="textarea" name="content_template" id="content_message" style="width: 275px;"><?=$rs['content'];?></textarea></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td>Số kí tự <span class="count" style="color: red;"></span> số tin nhắn <span class="mess_length" style="color: red;"></span></td>
-        </tr>
-        <tr>
-            <td align="right">
-                <input type="submit" name="save_bt" value="Cập nhật" class="btn"></td>
-            <td align="left">
-                <input type="button" name="back_bt" value="Quay lại" onclick="history.back();" class="btn">
-            </td>
-        </tr>
-    <?php endforeach;?>
-    </table>
-    </form>
+    <fieldset>
+        <legend>Cập nhật mẫu tin nhắn</legend>
+        <form action="<?=base_url();?>message/template_update" method="post">
+        
+        <table width="60%" align="center" style="margin-left: 350px; padding-top: 30px;">   
+        <?php foreach($data as $rs):?>
+            <input type="hidden" name="template_id_txt" value="<?=$rs['template_id'];?>">        
+            <tr>
+                <th>Tiêu đề</th>
+                <td><input type="text" name="title" value="<?=$rs['title'];?>" style="width: 275px;"></td>
+            </tr>
+            <tr>
+                <th>Nội dung</th>
+                <td><textarea cols="50" rows="4" class="textarea" name="content_template" id="content_message" style="width: 275px;"><?=$rs['content'];?></textarea></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>Số kí tự <span class="count" style="color: red;"></span> số tin nhắn <span class="mess_length" style="color: red;"></span></td>
+            </tr>
+            <tr>
+                <td align="right">
+                    <input type="submit" name="save_bt" value="Cập nhật" class="btn"></td>
+                <td align="left">
+                    <input type="button" name="back_bt" value="Quay lại" onclick="history.back();" class="btn">
+                </td>
+            </tr>
+        <?php endforeach;?>
+        </table>
+        </form>
+    </fieldset>
 </div>
